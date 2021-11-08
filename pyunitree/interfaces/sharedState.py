0015a81d0ef4b028609a8a08fca9a057c1836d76
Eduard Zalyaev <edzaljaev@gmail.com~>
2021-11-08 11:18:23 +0300

    Moved shared state to unitreepy

---
+++ pyunitree/interfaces/sharedState.py
@@ -0,0 +1,252 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+from pyunitree.utils.kinematics import FootPositionInHipFrameToJointAngle,FootPositionInHipFrame,EulerFromQuaternion
+from pyunitree.utils.gaitStates import LegState
+from pyunitree.robots.a1.constants import POSITION_GAINS,DAMPING_GAINS
+from pyunitree.base.daemon import SHM_IMPORTED
+from pyunitree.parsers.remote import WirelessRemote
+
+class A1SharedState:
+    MPC_BODY_MASS = 108 / 9.8
+    MPC_BODY_INERTIA = np.array((0.24, 0, 0, 0, 0.80, 0, 0, 0, 1.00))
+
+    COM_OFFSET = -np.array([0.012731, 0.002186, 0.000515])
+    HIP_OFFSETS = np.array([[0.183, -0.047, 0.], [0.183, 0.047, 0.],
+                            [-0.183, -0.047, 0.], [-0.183, 0.047, 0.]
+                            ]) + COM_OFFSET
+
+    _DEFAULT_HIP_POSITIONS = (
+        (0.17, -0.135, 0),
+        (0.17, 0.13, 0),
+        (-0.195, -0.135, 0),
+        (-0.195, 0.13, 0),
+    )
+    
+    MOTOR_DIRECTION = np.ones(12)
+
+    KPS = POSITION_GAINS
+    KDS = DAMPING_GAINS
+
+    _STANCE_DURATION_SECONDS = [
+        0.3
+    ] * 4  # For faster trotting (v > 1.5 ms reduce this to 0.13s).
+
+    #Standing
+    #_DUTY_FACTOR = [1.] * 4
+    #_INIT_PHASE_FULL_CYCLE = [0., 0., 0., 0.]
+
+    #_INIT_LEG_STATE = (
+    #     LegState.STANCE,
+    #     LegState.STANCE,
+    #     LegState.STANCE,
+    #     LegState.STANCE,
+    #)
+
+
+    # Tripod
+    # _DUTY_FACTOR = [.8] * 4
+    # _INIT_PHASE_FULL_CYCLE = [0., 0.25, 0.5, 0.]
+
+    # _INIT_LEG_STATE = (
+    #     LegState.STANCE,
+    #     LegState.STANCE,
+    #     LegState.STANCE,
+    #     LegState.SWING,
+    # )
+
+    # Trotting
+    _DUTY_FACTOR = [0.6] * 4
+    _INIT_PHASE_FULL_CYCLE = [0.9, 0, 0, 0.9]
+
+    _INIT_LEG_STATE = (
+        LegState.SWING,
+        LegState.STANCE,
+        LegState.STANCE,
+        LegState.SWING,
+    )
+
+    MPC_BODY_HEIGHT = 0.24
+    MPC_VELOCITY_MULTIPLIER = 0.5
+    FOOT_FORCE_THRESHOLD = 10
+    
+    DATA_TYPE = np.float32
+    """
+    self.stateVec
+     quaternion  |4|
+           gyro  |3|
+          accel  |3|
+     footforces  |4|
+              q  |12|
+             dq  |12|
+            tick |1|
+    """
+    def __init__(self,stateBufferPtr=None,modelBufferPtr=None,observerBufferPtr=None,wirelessRemotePtr=None):
+
+        if SHM_IMPORTED:
+            from multiprocessing.shared_memory import SharedMemory
+            self.rawStateShm = SharedMemory(name="RobotState")
+            self.stateVec = np.frombuffer(self.rawStateShm.buf, dtype=self.DATA_TYPE)
+
+            try:
+                self.rawJacShm = SharedMemory(name="Model")
+                self.rawJacobiansBuffer = np.frombuffer(self.rawJacShm.buf, dtype=self.DATA_TYPE)
+            except FileNotFoundError:
+                self.rawJacobiansBuffer = None
+
+            try:
+                self.rawVelShm = SharedMemory(name="Observer")
+                self.rawVelocitiesBuffer = np.frombuffer(self.rawVelShm.buf, dtype=self.DATA_TYPE)
+            except:
+                self.rawVelocitiesBuffer = None
+
+            try:
+                self.rawRemoteShm = SharedMemory(name="WirelessRemote")
+                self.rawRemoteBuffer =  np.frombuffer(self.rawRemoteShm.buf,dtype=np.bytes_)
+            except:
+                self.rawRemoteBuffer = None
+        else:
+            from multiprocessing import RawArray
+            if stateBufferPtr is not None:
+                self.stateVec = np.frombuffer(stateBufferPtr, dtype=self.DATA_TYPE)
+
+            if modelBufferPtr is not None:
+                self.rawJacobiansBuffer = np.frombuffer(modelBufferPtr, dtype=self.DATA_TYPE)
+            
+            if observerBufferPtr is not None:
+                self.rawVelocitiesBuffer = np.frombuffer(observerBufferPtr, dtype=self.DATA_TYPE)
+
+            if wirelessRemotePtr is not None:
+                self.rawRemoteBuffer = np.frombuffer(wirelessRemotePtr, dtype=np.float32)
+
+        if wirelessRemotePtr is not None:
+            self.wirelessParser = WirelessRemote()
+
+    def __del__(self):
+
+        if SHM_IMPORTED:
+
+            if self.rawStateShm is not None:
+                self.rawStateShm.close()
+            
+            if self.rawVelShm is not None:
+                self.rawVelShm.close()
+
+            if self.rawJacShm is not None:
+                self.rawJacShm.close()
+
+            if self.rawRemoteShm is not None:
+                self.rawRemoteShm.close()
+
+    def GetBaseOrientationQuaternion(self):
+        q = self.stateVec[:4]
+        return [q[1],q[2],q[3],q[0]]
+    
+    def GetBaseOrientationMatrix(self):
+        return R.from_quat(self.GetBaseOrientationQuaternion()).as_matrix()
+
+    def GetBaseRollPitchYaw(self):
+        return EulerFromQuaternion(self.GetBaseOrientationQuaternion())
+
+    def GetBaseRollPitchYawRate(self):
+        return self.stateVec[4:7]
+
+    def GetBaseAcceleration(self):
+        return self.stateVec[7:10]
+
+    def GetFootForces(self):
+        return self.stateVec[10:14]
+
+    def GetJointAngles(self):
+        return self.stateVec[14:26]
+
+    def GetMotorVelocities(self):
+        return self.stateVec[26:38]
+    
+    def GetCurrentTick(self):
+        return self.stateVec[38]
+
+    def GetFootPositionsInBaseFrame(self):
+        foot_angles = self.GetJointAngles()
+        foot_angles = np.reshape(foot_angles,(4,3))
+        footPositions = np.zeros((4, 3))
+
+        for i in range(4):
+            footPositions[i] = FootPositionInHipFrame(foot_angles[i],
+                                                        l_hip_sign=i)
+        return footPositions + self.HIP_OFFSETS
+        
+    def ComputeMotorAnglesFromFootLocalPosition(self, leg_id,
+                                                foot_local_position):
+                                                
+        joint_position_idxs = list(range(leg_id * 3,leg_id * 3 + 3))
+
+        joint_angles = FootPositionInHipFrameToJointAngle(
+            foot_local_position - self.HIP_OFFSETS[leg_id],
+            l_hip_sign=leg_id)
+
+        joint_angles = np.multiply(
+            joint_angles,
+            self.MOTOR_DIRECTION[joint_position_idxs])
+
+        return joint_position_idxs, joint_angles.tolist()
+
+
+    def GetHipPositionsInBaseFrame(self):
+        return self._DEFAULT_HIP_POSITIONS
+
+    def GetMotorPositionGains(self):
+        return self.KPS
+
+    def GetMotorVelocityGains(self):
+        return self.KDS
+
+    def GetFootContacts(self):
+        footForce = self.GetFootForces()
+        return np.array(footForce) > self.FOOT_FORCE_THRESHOLD
+    
+    def GetVelocities(self):
+        velocities = self.rawVelocitiesBuffer
+        return velocities.reshape((2,3))
+
+    def GetJacobians(self):
+        jacobians = self.rawJacobiansBuffer
+        return jacobians.reshape((12,3))
+
+    def GetBaseVelocity(self):
+        return self.GetVelocities()[0]
+    
+    def GetComVelocity(self):
+        return self.GetVelocities()[1]
+
+    def ComputeJacobian(self,idx):
+        return self.GetJacobians()[idx*3:idx*3+3]
+    
+    def printWirelessState(self):
+        remote = self.wirelessParser
+        state = np.copy(self.rawRemoteBuffer).tobytes()
+        remote.set_state(state)
+        remote.update_state()
+        print(f'\n{15*"/"} WIRELESS REMOTE STATE {15*"/"}')
+        print(f' BUTTONS:\n A {remote.button.A} B {remote.button.B} X {remote.button.X} Y {remote.button.Y}',
+            f'\n  Right {remote.button.right} Left {remote.button.left} Up {remote.button.up} Down {remote.button.down}',
+            f'\n  R1 {remote.button.R1} R2 {remote.button.R2} L1 {remote.button.L1} L2 {remote.button.L2}',
+            f'\n  Start {remote.button.start} Select {remote.button.select} F1 {remote.button.F1} F2 {remote.button.F2}',
+            f'\n JOYSTICKS:\n LEFT: X {round(remote.left_joystick[0],2)} Y {round(remote.left_joystick[1],2)}',
+            f'\n  RIGHT: X {round(remote.right_joystick[0],2)}  Y {round(remote.right_joystick[1],2)}',
+            end=10*" " + "\n", flush=True)
+        print(f'{52*"/"}\n')
+        print(12*'\033[A', end="\r", flush=True)
+
+    def printfullState(self):
+        print("GetBaseOrientationQuaternion")
+        print(self.GetBaseOrientationQuaternion())
+        print("GetBaseAcceleration")
+        print(self.GetBaseAcceleration())
+        print("GetFootForces")
+        print(self.GetFootForces())
+        print("GetJointAngles")
+        print(self.GetJointAngles())
+        print("GetMotorVelocities")
+        print(self.GetMotorVelocities())
+        print("GetBaseVelocity")
+        print(self.GetBaseVelocity())
--- pyunitree/utils/kinematics.py
+++ pyunitree/utils/kinematics.py
@@ -1,4 +1,5 @@
-from numpy import sin, cos, array
+import numpy as np
+import math
 
 def leg_kinematics(motor_angles, link_lengths, base_position):
 
@@ -6,23 +7,130 @@ def leg_kinematics(motor_angles, link_lengths, base_position):
     r0_x, r0_y = base_position
     l1, l2, l3 = link_lengths
 
-    c1, s1 = cos(q1), sin(q1)
-    c2, s2 = cos(q2), sin(q2)
-    c23, s23 = cos(q2 + q3), sin(q2 + q3)
+    c1, s1 = np.cos(q1), np.sin(q1)
+    c2, s2 = np.cos(q2), np.sin(q2)
+    c23, s23 = np.cos(q2 + q3), np.sin(q2 + q3)
 
     # calculate the position of the foot
-    position = array([-l2*s2 - l3*s23 + r0_x,
+    position = np.array([-l2*s2 - l3*s23 + r0_x,
                       -l1*c1 + (l2*c2 + l3*c23)*s1 + r0_y,
                       -l1*s1 - (l2*c2 + l3*c23)*c1])
 
     # jacobian of the foot position with respect to
-    jacobian = array([[0, -l2*c2 - l3*c23, -l3*c23],
+    jacobian = np.array([[0, -l2*c2 - l3*c23, -l3*c23],
                       [l1*s1 + (l2*c2 + l3*c23)*c1, -
                           (l2*s2 + l3*s23)*s1, -l3*s1*s23],
                       [-l1*c1 + (l2*c2 + l3*c23)*s1, (l2*s2 + l3*s23)*c1, l3*s23*c1]])
 
-    rotation_matrix = array([[c23, s1*s23, -s23*c1],
+    rotation_matrix = np.array([[c23, s1*s23, -s23*c1],
                              [0, c1, s1],
                              [s23, -s1*c23, c1*c23]])
 
     return position, jacobian, rotation_matrix
+
+def EulerFromQuaternion(quat):
+        x,y,z,w = quat
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return [roll_x, pitch_y, yaw_z]
+
+def RotFromQuaternion(quat):
+    q0 = quat[0]
+    q1 = quat[1]
+    q2 = quat[2]
+    q3 = quat[3]
+     
+    # First row of the rotation matrix
+    r00 = 2 * (q0 * q0 + q1 * q1) - 1
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+     
+    # Second row of the rotation matrix
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = 2 * (q0 * q0 + q2 * q2) - 1
+    r12 = 2 * (q2 * q3 - q0 * q1)
+     
+    # Third row of the rotation matrix
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = 2 * (q0 * q0 + q3 * q3) - 1
+     
+    return np.array([[r00, r01, r02],
+                    [r10, r11, r12],
+                    [r20, r21, r22]])
+
+def FootPositionInHipFrame(angles, l_hip_sign=1):
+    theta_ab, theta_hip, theta_knee = angles[0], angles[1], angles[2]
+    l_up = 0.2
+    l_low = 0.2
+    l_hip = 0.08505 * ((-1)**(l_hip_sign + 1))
+    leg_distance = np.sqrt(l_up**2 + l_low**2 +
+                            2 * l_up * l_low * math.cos(theta_knee))
+    eff_swing = theta_hip + theta_knee / 2
+
+    off_x_hip = -leg_distance * math.sin(eff_swing)
+    off_z_hip = -leg_distance * math.cos(eff_swing)
+    off_y_hip = l_hip
+
+    theta_ab_cos = math.cos(theta_ab)
+    theta_ab_sin = math.sin(theta_ab)
+    
+    off_x = off_x_hip
+    off_y = theta_ab_cos * off_y_hip - theta_ab_sin * off_z_hip
+    off_z = theta_ab_sin * off_y_hip + theta_ab_cos * off_z_hip
+    return [off_x, off_y, off_z]
+
+def FootPositionInHipFrameToJointAngle(foot_position, l_hip_sign=1):
+    l_up = 0.2
+    l_low = 0.2
+    l_hip = 0.08505 * ((-1)**(l_hip_sign + 1))
+    x, y, z = foot_position[0], foot_position[1], foot_position[2]
+    theta_knee = -np.arccos(
+        (x**2 + y**2 + z**2 - l_hip**2 - l_low**2 - l_up**2) /
+        (2 * l_low * l_up))
+    l = np.sqrt(l_up**2 + l_low**2 + 2 * l_up * l_low * np.cos(theta_knee))
+    theta_hip = np.arcsin(-x / l) - theta_knee / 2
+    c1 = l_hip * y - l * np.cos(theta_hip + theta_knee / 2) * z
+    s1 = l * np.cos(theta_hip + theta_knee / 2) * y + l_hip * z
+    theta_ab = np.arctan2(s1, c1)
+    return np.array([theta_ab, theta_hip, theta_knee])
+
+def AnalyticalLegJacobian(leg_angles, sign):
+    """
+    Computes the analytical Jacobian.
+    Args:
+    ` leg_angles: a list of 3 numbers for current abduction, hip and knee angle.
+    sign: whether it's a left (1) or right(-1) leg.
+    """
+    l_up = 0.2
+    l_low = 0.2
+    l_hip = 0.08505 * (-1)**(sign + 1)
+
+    t1, t2, t3 = leg_angles[0], leg_angles[1], leg_angles[2]
+    l_eff = np.sqrt(l_up**2 + l_low**2 + 2 * l_up * l_low * np.cos(t3))
+    t_eff = t2 + t3 / 2
+    J = np.zeros((3, 3))
+    J[0, 0] = 0
+    J[0, 1] = -l_eff * np.cos(t_eff)
+    J[0, 2] = l_low * l_up * np.sin(t3) * np.sin(t_eff) / l_eff - l_eff * np.cos(
+        t_eff) / 2
+    J[1, 0] = -l_hip * np.sin(t1) + l_eff * np.cos(t1) * np.cos(t_eff)
+    J[1, 1] = -l_eff * np.sin(t1) * np.sin(t_eff)
+    J[1, 2] = -l_low * l_up * np.sin(t1) * np.sin(t3) * np.cos(
+        t_eff) / l_eff - l_eff * np.sin(t1) * np.sin(t_eff) / 2
+    J[2, 0] = l_hip * np.cos(t1) + l_eff * np.sin(t1) * np.cos(t_eff)
+    J[2, 1] = l_eff * np.sin(t_eff) * np.cos(t1)
+    J[2, 2] = l_low * l_up * np.sin(t3) * np.cos(t1) * np.cos(
+        t_eff) / l_eff + l_eff * np.sin(t_eff) * np.cos(t1) / 2
+    return J
