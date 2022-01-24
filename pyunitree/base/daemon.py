Fixed shared memory leakage

pyunitree/base/daemon.py
@@ -41,15 +41,15 @@ class Daemon:
         self.handlerProc = Process(target=self.handler,daemon=True)
         self.updateRate = updateRate
         self.hasSharedState = False
-        self._processReady = RawValue("b",0)
+        self.sh_processRunning = RawValue("b",0)
 
     def handler(self):
         self.processInit()
         try:
-            self._processReady.value = 1
+            self.sh_processRunning.value = 1
             initial_time = perf_counter()
             tick = 0
-            while True:
+            while self.sh_processRunning.value:
                 actual_time = perf_counter() - initial_time
                 if actual_time - tick >= 1/self.updateRate or self.updateRate<0:
                     result = self.action()
@@ -67,10 +67,7 @@ class Daemon:
             exception = traceback.format_exception(type(e), e, e.__traceback__)
             info(f"Daemon process {self.name} was interrupted by an exception inside the handler \n \
                 Exception: \n {''.join(exception)}")
-
-
-
-
+        
     def initSharedStateArray(self,size,name,dataType=np.float32):
         self.hasSharedState = True
         self.sharedStateName = name
@@ -97,7 +94,7 @@ class Daemon:
     def start(self):
         self.preStart()
         self.handlerProc.start()
-        while self._processReady.value == 0:
+        while self.sh_processRunning.value == 0:
             sleep(0.01)
         info(f"Process {self.name} has started")
         self.onStart()
@@ -107,20 +104,22 @@ class Daemon:
     
     def stop(self):
         self.onStop()
-        self.handlerProc.terminate()
+        self.sh_processRunning.value = 0
+        self.handlerProc.join(timeout=1)
+        self.cleanup()
         info(f"Process {self.name} terminated")
 
-    def __del__(self):
-        if self.handlerProc.is_alive():
-            self.stop()
-            
+    def unlinkSharedMemory(self):       
         if self.hasSharedState == True and SHM_IMPORTED:
-            self.rawStateShm.close()
             try:
                 self.rawStateShm.unlink()
             except:
                 info(f"Process {self.name} failed to unlink shared_memory")
 
+    def cleanup(self):
+        if hasattr(self,"context"):
+            self.context.unlinkSharedMemory()
+        self.unlinkSharedMemory()
     
     def init(self):
         pass

pyunitree/interfaces/sharedState.py
@@ -13,6 +13,7 @@ class A1SharedState:
     #original motion imitation
     MPC_BODY_MASS = 108 / 9.8
     MPC_BODY_INERTIA = np.array((0.24, 0, 0, 0, 0.80, 0, 0, 0, 1.00))
+    SHM_NAMES = ['rawStateShm','rawObserverShm','rawModelShm','rawRemoteShm','rawCommandShm']
     # This is actually default foot positions in robot CS
     _DEFAULT_HIP_POSITIONS = (
         (0.17, -0.135, 0),
@@ -118,27 +119,28 @@ class A1SharedState:
         self.remoteCopy = np.zeros(self.rawRemoteBuffer.shape) if self.rawRemoteBuffer is not None else None
         self.commandCopy = np.zeros(self.rawCommandBuffer.shape) if self.rawCommandBuffer is not None else None
 
-    def __del__(self):
+    def unlinkSharedMemory(self):
         if SHM_IMPORTED: 
-            if hasattr(self, 'rawStateShm'):
-                self.rawStateShm.close()
-            
-            if hasattr(self, 'rawObserverShm'):
-                self.rawObserverShm.close()
-
-            if hasattr(self, 'rawModelShm'):
-                self.rawModelShm.close()
-
-            if hasattr(self, 'rawRemoteShm'):
-                self.rawRemoteShm.close()
-            
-            if hasattr(self,'rawCommandShm'):
-                self.rawCommandShm.close()
-                if self.createdCommandShm:
+            for shm_name in self.SHM_NAMES:
+                if hasattr(self, shm_name):
                     try:
-                        self.rawCommandShm.unlink()
-                    except:
-                        info("A1SharedState failed to unlink shared memory containing the command")
+                        getattr(self,shm_name).unlink()
+                    except FileNotFoundError:
+                        info("UNLINK: Shared memory {shm_name} block not found")
+
+    def destroySharedMemory(self):
+        if SHM_IMPORTED: 
+            for shm_name in self.SHM_NAMES:
+                if hasattr(self, shm_name):
+                    try:
+                        getattr(self,shm_name).close()
+                    except FileNotFoundError:
+                        info("DESTROY: Shared memory {shm_name} block not found")
+
+    def cleanup(self,destroy=False):
+        self.unlinkSharedMemory()
+        if destroy:
+            self.destroySharedMemory()
 
     def copyBuffers(self,buffers=["state","model","observer","remote","command"]):
         if self.rawStateBuffer is not None and "state" in buffers: